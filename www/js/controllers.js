7d9b593c51158f544f84d71855b458f740273f61
Jay Milnamow <jaymilnamow@gmail.com>
2016-12-23 05:54:07 -0500

    Twitter Authentication almost debugged

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -49,27 +49,27 @@ app.controller('TwitterController', function($scope, $q, twitterService) {
     }
 });
 
-
-
-angular.module('starter.controllers', [])
-
-.controller('ScheduledTweetsCtrl', function($scope) {
-  init = function(){
-    console.log("STC");
-  }
-  init();
-})
-
-.controller('AddNewCtrl', function($scope) {
-  init = function(){
-    console.log("ANC");
-  }
-  init();
-})
-
-.controller('ScheduleCtrl', function($scope) {
-  init = function(){
-    console.log("SC");
-  }
-  init();
-})
+//
+//
+//angular.module('starter.controllers', [])
+//
+//.controller('ScheduledTweetsCtrl', function($scope) {
+//  init = function(){
+//    console.log("STC");
+//  }
+//  init();
+//})
+//
+//.controller('AddNewCtrl', function($scope) {
+//  init = function(){
+//    console.log("ANC");
+//  }
+//  init();
+//})
+//
+//.controller('ScheduleCtrl', function($scope) {
+//  init = function(){
+//    console.log("SC");
+//  }
+//  init();
+//})
